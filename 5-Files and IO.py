c126f7905830358dd1bc8621c2b9f664e2dc4956
netager1007 <thinkjbank@gmail.com>
2020-03-12 18:29:58 +0900

    2020.03.12-2

--- 5-Files and IO.py
+++ 5-Files and IO.py
@@ -284,3 +284,255 @@ print(s.getvalue())
 # Be aware that StringIO and BytesIO instances don’t have a proper integer filedescriptor.
 # Thus, they do not work with code that requires the use of a real system-level
 # file such as a file, pipe, or socket.
+
+# 5.7 Reading and Writing Compressed Datafiles
+
+# Problem
+# You need to read or write data in a file with gzip or bz2 compression.
+
+# Solution
+# The gzip and bz2 modules make it easy to work with such files. Both modules provide
+# an alternative implementation of open() that can be used for this purpose. For example,
+# to read compressed files as text, do this:
+
+# gzip compression
+import gzip
+
+# gzip compression
+import gzip
+
+with gzip.open('somefile.gz', 'wt') as f:
+    f.write('abc')
+
+with gzip.open('somefile.gz', 'rt') as f:
+    text = f.read()
+
+# bz2 compression
+import bz2
+
+with bz2.open('somefile.bz2', 'wt') as f:
+    f.write('abc')
+
+with bz2.open('somefile.bz2', 'rt') as f:
+    text = f.read()
+
+
+# Discussion
+# For the most part, reading or writing compressed data is straightforward. However, be
+# aware that choosing the correct file mode is critically important. If you don’t specify a
+# mode, the default mode is binary, which will break programs that expect to receive text.
+# Both gzip.open() and bz2.open() accept the same parameters as the built-in open()
+# function, including encoding, errors, newline, and so forth.
+
+# When writing compressed data, the compression level can be optionally specified using
+# the compresslevel keyword argument. For example:
+with gzip.open('somefile.gz', 'wt', compresslevel=5) as f:
+    f.write('abcdef')
+
+# The default level is 9, which provides the highest level of compression. Lower levels offer
+# better performance, but not as much compression.
+# Finally, a little-known feature of gzip.open() and bz2.open() is that they can be layered
+# on top of an existing file opened in binary mode. For example, this works:
+
+import gzip
+f = open('somefile.gz', 'rb')
+with gzip.open(f, 'rt') as g:
+    text = g.read()
+print(text)
+
+import gzip
+with open('somefile.gz', 'rb') as f:
+    with gzip.open(f, 'rt') as g:
+        text = g.read()
+print(text)
+
+# Discussion
+# A little-known feature of the iter() function is that it can create an iterator if you pass
+# it a callable and a sentinel value. The resulting iterator simply calls the supplied callable
+# over and over again until it returns the sentinel, at which point iteration stops.
+# In the solution, the functools.partial is used to create a callable that reads a fixed
+# number of bytes from a file each time it’s called. The sentinel of b'' is what gets returned
+# when a file is read but the end of file has been reached.
+# Last, but not least, the solution shows the file being opened in binary mode. For reading
+# fixed-sized records, this would probably be the most common case. For text files, reading
+# line by line (the default iteration behavior) is more common.
+
+
+# 5.9 Reading Binary Data into a Mutable Buffer
+
+# Problem
+# You want to read binary data directly into a mutable buffer without any intermediate
+# copying. Perhaps you want to mutate the data in-place and write it back out to a file.
+
+# Solution
+# To read data into a mutable array, use the readinto() method of files. For example:
+import os.path
+
+def read_into_buffer(filename):
+    buf = bytearray(os.path.getsize(filename))
+    with open(filename, 'rb') as f:
+        f.readinto(buf)
+    return buf
+
+# Write a sample file
+with open('somefile.bin', 'wb') as f:
+    f.write(b'Hello World')
+buf = read_into_buffer('somefile.bin')
+print(buf)
+buf[0:5] = b'Hallo'
+print(buf)
+
+with open('newsample.bin', 'wb') as f:
+    f.write(buf)
+
+# Discussion
+# The readinto() method of files can be used to fill any preallocated array with data. This
+# even includes arrays created from the array module or libraries such as numpy. Unlike
+# the normal read() method, readinto() fills the contents of an existing buffer rather
+# than allocating new objects and returning them. Thus, you might be able to use it to
+# avoid making extra memory allocations. For example, if you are reading a binary file
+# consisting of equally sized records, you can write code like this:
+
+record_size = 32       # Size of each record(adjust value)
+
+buf = bytearray(record_size)
+with open('somefile', 'rb') as f:
+    while True:
+        n = f.readinto(buf)    # Return : number of bytes actually read
+        if n < record_size:
+            break
+        # Use the contents of file
+
+# Another interesting feature to use here might be a memoryview, which lets you make
+# zero-copy slices of an existing buffer and even change its contents. For example:
+print(buf)
+m1 = memoryview(buf)
+m2 = m1[-5:]
+print(m2)
+print(m2[:])
+print(buf)
+
+# One caution with using f.readinto() is that you must always make sure to check its
+# return code, which is the number of bytes actually read.
+# If the number of bytes is smaller than the size of the supplied buffer, it might indicate
+# truncated or corrupted data (e.g., if you were expecting an exact number of bytes to be
+# read).
+# Finally, be on the lookout for other “into” related functions in various library modules
+# (e.g., recv_into(), pack_into(), etc.). Many other parts of Python have support for
+# direct I/O or data access that can be used to fill or alter the contents of arrays and buffers.
+# See Recipe 6.12 for a significantly more advanced example of interpreting binary structures
+# and usage of memoryviews.
+
+
+# 5.10 Memory Mapping Binary Files
+
+# Problem
+# You want to memory map a binary file into a mutable byte array, possibly for random
+# access to its contents or to make in-place modifications.
+
+# Solution
+# Use the mmap module to memory map files. Here is a utility function that shows how to
+# open a file and memory map it in a portable manner:
+
+import os
+import mmap
+
+def memory_map(filename, access=mmap.ACCESS_WRITE):
+    size = os.path.getsize(filename)
+    fd = os.open(filename, os.O_RDWR)
+    return mmap.mmap(fd, size, access=access)
+
+# To use this function, you would need to have a file already created and filled with data.
+# Here is an example of how you could initially create a file and expand it to a desired
+# size:
+size = 1000000
+with open('data', 'wb') as f:
+    f.seek(size-1)
+    f.write(b'\x00')
+
+m = memory_map('data')
+print(len(m))
+print(m[0:10])
+print(m[0])
+
+m[0:11] = b'Hello World'       # Reassign a slice
+print(m.closed)
+m.close()
+
+with open('data', 'rb') as f:  # Verify that changes were made
+    print(f.read(11))
+
+
+with memory_map('data') as m:
+    print(len(m))
+    print(m[0:10])
+print(m.closed)
+
+# By default, the memory_map() function shown opens a file for both reading and writing.
+# Any modifications made to the data are copied back to the original file. If read-only
+# access is needed instead, supply mmap.ACCESS_READ for the access argument. For
+# example:
+with memory_map('newsample.bin', mmap.ACCESS_READ) as m:
+    print(len(m))
+    print(m[0:11])
+
+# Discussion
+# Using mmap to map files into memory can be an efficient and elegant means for randomly
+# accessing the contents of a file. For example, instead of opening a file and performing
+# various combinations of seek(), read(), and write() calls, you can simply map the
+# file and access the data using slicing operations.
+# Normally, the memory exposed by mmap() looks like a bytearray object. However, you
+# an interpret the data differently using a memoryview. For example:
+m = memory_map('data')
+v = memoryview(m).cast('I')
+v[0] = 7
+print(m[0:4])
+m[0:4] = b'\x07\x01\x00\x00'
+print(v[0])
+
+
+# 5.11 Manipulating Pathnames
+# Problem
+# You need to manipulate pathnames in order to find the base filename, directory name,
+# absolute path, and so on.
+
+import os
+path = '/User/JBB/Data/data.csv'
+print(os.path.basename(path))   # Return 'data.csv' : Get the last component of the path
+print(os.path.dirname(path))    # Get the directory name
+print(os.path.join('tmp', 'data', os.path.basename(path))) # Join path components together
+                                                           # Return : /tmp/data/data.csv
+print(os.path.join('tmp', 'data', 'data.csv'))
+path = '~/Data/data.csv'
+print(os.path.expanduser(path))  # Expand the user's home directory
+print(os.path.splitext(path))    # Split the file extension
+
+# Discussion
+# For any manipulation of filenames, you should use the os.path module instead of trying
+# to cook up your own code using the standard string operations. In part, this is for
+# portability. The os.path module knows about differences between Unix and Windows
+# and can reliably deal with filenames such as Data/data.csv and Data\data.csv. Second,
+# you really shouldn’t spend your time reinventing the wheel. It’s usually best to use the
+# functionality that’s already provided for you.
+
+
+# 5.12 Testing for the Existence of a File
+import os
+print(os.path.exists('test.txt'))
+print(os.path.exists('/tmp/spam'))
+
+print(os.path.isfile('test.txt'))  # Is a regular file
+print(os.path.isdir('test.txt'))   # Is a directory
+print(os.path.islink('test.txt'))   # Is a symbolic link
+print(os.path.realpath('test.txt'))   # Get the file linked to
+
+# If you need to get metadata (e.g., the file size or modification date), that is also available
+# in the os.path module.
+print(os.path.getsize('test.txt'))
+
+print(os.path.getmtime('test.txt'))
+import time
+print(time.ctime(os.path.getmtime('test.txt')))
+
+
+
